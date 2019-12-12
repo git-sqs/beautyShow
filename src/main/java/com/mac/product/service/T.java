b7a5cb876bb0676d774ff45398429b50c279a66f
sily-wei <sily_wei@163.com>
2019-12-12 11:25:06 +0800

    由于gitHub 上传空文件夹 认为没有东西 所以创建T class 写的时候删了 换成自己的就行

---
+++ src/main/java/com/mac/product/service/T.java
@@ -0,0 +1,8 @@
+package com.mac.product.service;
+
+/**
+ * @author weining
+ * @date 2019/12/12 11:22
+ */
+public class T {
+}
---
+++ src/main/java/com/mac/user/dao/T.java
@@ -0,0 +1,8 @@
+package com.mac.user.dao;
+
+/**
+ * @author weining
+ * @date 2019/12/12 11:22
+ */
+public class T {
+}
