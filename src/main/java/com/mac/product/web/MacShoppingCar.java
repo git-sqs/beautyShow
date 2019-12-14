87cba8ce4855ab1d243f74259b1eddac38da76cb
sily-wei <sily_wei@163.com>
2019-12-14 11:38:18 +0800

    修改购物车的查询功能

--- src/main/java/com/mac/product/web/MacShoppingCar.java
+++ src/main/java/com/mac/product/web/MacShoppingCar.java
@@ -1,13 +1,10 @@
 package com.mac.product.web;
 
 import com.mac.common.vo.ResultVo;
-import com.mac.product.entity.Mac_Goods;
-import com.mac.product.service.Mac_ShoppingCarService;
+import com.mac.product.service.MacShoppingCarService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.models.auth.In;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,26 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Api(value = "购物车的实现",tags = "购物车的实现")
 @RestController
-public class Mac_ShoppingCar {
+public class MacShoppingCar {
 
     @Autowired
-    private Mac_ShoppingCarService mac_shoppingCarService;
+    private MacShoppingCarService macShoppingCarService;
 
     @ApiOperation(value = "通过Id查询商品数据",notes ="通过Id查询商品数据" )
-    @GetMapping("/api/product/findByGoodsId.do")
+    @GetMapping("/mac/product/findByGoodsId.do")
     public ResultVo findByGoodsId(Integer id){
-        return mac_shoppingCarService.findByGoodsId(id);
+        return macShoppingCarService.findByGoodsId(id);
     }
 
     @ApiOperation(value = "通过Id查询指定商品颜色",notes = "通过Id查询指定商品颜色")
-    @GetMapping("/api/product/findColorByGoodsId.do")
+    @GetMapping("/mac/product/findColorByGoodsId.do")
     public ResultVo findColorByGoodsId(Integer id){
-        return mac_shoppingCarService.findColorByGoodsId(id);
+        return macShoppingCarService.findColorByGoodsId(id);
     }
 
     @ApiOperation(value = "通过Id查询指定商品类型",notes = "通过Id查询指定商品类型")
-    @GetMapping("/api/product/findTypeByGoodsId.do")
+    @GetMapping("/mac/product/findTypeByGoodsId.do")
     public ResultVo findTypeByGoodsId(Integer id){
-        return mac_shoppingCarService.findTypeByGoodsId(id);
+        return macShoppingCarService.findTypeByGoodsId(id);
     }
 }
